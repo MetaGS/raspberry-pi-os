257c0c2fb0dc99b021649464ce68713fbb5c58a0
Beck TJ (Bakytbek Tatibekov) <22246818+beckinfonet@users.noreply.github.com>
2019-09-16 00:23:46 -0400

    Update print.js

--- print.js
+++ print.js
@@ -58,3 +58,42 @@ function traverse(node) {
   return tree;
 }
 
+/*
+1. Create a new Node by passing a value
+2. Check if there is a root node, if it is null, then set it to newNode
+3. BUT, if THERE is a root already, then --> currentNode === this.root,
+   because we are going to traverse it. 
+   We will use while loop since we do not know how long we are going to 
+   traverse it for. 
+
+4. Now we found out that there is a root node, we check if the value we have 
+   is smaller than the root node:
+   if(value < currentNode.value) ?
+
+   IF it is smaller, then we go LEFT.
+
+   Here we do one of two things:
+      a) if there is no currentNode on the left, assign newNode as a new value
+         by saying: 
+         if(!currentNode.left) {
+           currentNode.left = newNode;
+           return this;
+         }
+
+      b) However, if there is something to the left, 
+         then continue traversing by using recursion:
+         currentNode = currentNode.left;
+
+5. Second scenario, IF the value is greater than the current node, GO RIGHT. 
+
+    Here again we write two scenarios:
+      a) if there is no right node:
+         if(!currentNode.right) {
+           currentNode.right = newNode;
+           return this;
+         }
+      b) IF there is already a right node, then we will keep traversing until 
+         there is no node. 
+         currentNode = currentNode.right;
+*/  
+
